81e94fcc4f3430dd6ce3d3ace857737b29c12e84
lvzhengxu <lvzhengxu1987@126.com>
2017-03-07 22:24:44 +0800

    update

--- ProjectEuler/ab.py
+++ ProjectEuler/ab.py
@@ -1,21 +1,22 @@
-
-def Fibonacci(n):
+def FFibonacci(n):
     if n == 1 or n == 0:
         return 1
-    return Fibonacci(n-1) + n
+    return FFibonacci(n-1) + n
 
-#print Fibonacci(10)
+print FFibonacci(999)
+'''
 i = 0
+sum = 0
 while True:
-    value = Fibonacci(i)
+    value = FFibonacci(i)
+    print value
     i += 1
-    
     if value % 2 == 0 and value <= 4000000 :
         sum += value
     if value > 4000000 :
         break
 print sum
-
+'''
 
 
 
